df5fa15b8c1088ef9f0ff58e22612c71e608c2a4
Astral-23 <ul7cr38@gmail.com>
2024-05-11 00:55:23 +0900

    hpp

--- verify/SegTree.test.cpp
+++ verify/SegTree.test.cpp
@@ -4,39 +4,6 @@ using namespace std;
 #include "../Utilities/modint998.hpp"
 #include "../DetaStructures/SegTree.hpp"
 
-struct mint{//998244353
-   long long x;
-   static const long long mod = 998244353;
-   mint(long long _x=0) noexcept : x((_x%mod+mod)%mod){}
-   long long val() const noexcept {return x;}
-   mint& operator+=(const mint& a) noexcept {if((x += a.x) >= mod) x -= mod;return *this;}
-   mint& operator-=(const mint& a) noexcept {if((x += mod - a.x) >= mod) x -= mod;return *this;}
-   mint& operator*=(const mint& a) noexcept {(x *= a.x) %= mod;return *this;}
-   mint& operator/=(const mint& a) {return (*this) *= a.inv();}
-   mint& operator++() noexcept {return *this += 1;}
-   mint& operator--() noexcept {return *this -= 1;}
-   mint operator++(int) {mint tmp = *this; ++*this; return tmp;}
-   mint operator--(int) {mint tmp = *this; --*this; return tmp;}
-   mint inv() const {return pow(mod-2);}
-   friend mint operator+(const mint&  t, const mint& a)  {mint tmp = t; return tmp += a;}
-   friend mint operator-(const mint&  t, const mint& a)  {mint tmp = t; return tmp += (a * -1);}
-   friend mint operator*(const mint&  t, const mint& a)  {mint tmp = t; return tmp *= a;}
-   friend mint operator/(const mint&  t, const mint& a)  {return a.inv() *=  t;}
-   mint pow(const long long& y) const {
-    if(!y) return 1;
-    mint res = pow(y >> 1);
-    res *= res;
-    if(y & 1) res *= *this;
-    return res;
-  }
-   friend istream& operator>>(istream& is, mint& a) { return is >> a.x;}
-   friend ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
-   bool operator==(const mint& a) const {return x == a.x;}
-   bool operator!=(const mint& a) const {return x != a.x;}
-};
-
-
-
 struct Monoid {
     mint a, b;
     
