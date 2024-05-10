468bb9452e7df684fd9c41f503f5b2448c803144
Astral-23 <ul7cr38@gmail.com>
2024-05-10 23:39:05 +0900

    modint

---
+++ Utilities/dynamic_modint2.cpp
@@ -0,0 +1,40 @@
+struct mint{//mint for big mod
+    long long x;
+    mint(long long _x=0) : x((_x%mod+mod)%mod){}
+    long long val()  {return x;}
+    mint& operator+=(const mint& a)  {if((x += a.x) >= mod) x -= mod;return *this;}
+    mint& operator-=(const mint& a)  {if((x += mod - a.x) >= mod) x -= mod;return *this;}
+    mint& operator*=(const mint& a)  {
+        __int128_t t = (__int128_t)(this -> x) * a.x;
+        t %= mod; if(t < 0) t += mod;
+        *this = t;
+        return *this;
+    }
+    mint& operator/=(const mint& a) {return (*this) *= a.inv();}
+    mint& operator++() noexcept {return *this += 1;}
+    mint& operator--() noexcept {return *this -= 1;}
+    mint operator++(int) {mint tmp = *this; ++*this; return tmp;}
+    mint operator--(int) {mint tmp = *this; --*this; return tmp;}
+    mint inv() const {return pow(mod-2);}
+    friend mint operator+(const mint&  t, const mint& a)  {mint tmp = t; return tmp += a;}
+    friend mint operator-(const mint&  t, const mint& a)  {mint tmp = t; return tmp += (a * -1);}
+    friend mint operator*(const mint&  t, const mint& a)  {mint tmp = t; return tmp *= a;}
+    friend mint operator/(const mint&  t, const mint& a)  {assert(a.x != 0) ; return a.inv() *=  t;}
+    mint pow(const long long& y) const {
+        assert(y >= 0);
+        if(!y) return 1;
+        mint res = pow(y >> 1);
+        res *= res;
+        if(y & 1) res *= *this;
+        return res;
+    }
+   friend istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+   friend ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+   bool operator==(const mint& a) const {return x == a.x;}
+   bool operator!=(const mint& a) const {return x != a.x;}
+   bool operator<(const mint& a) const {return x < a.x;}
+};
+
+/*
+@brief 任意modのmodint(mod < 2 ^ 64)
+*/
---
+++ Utilities/modint107.cpp
@@ -0,0 +1,38 @@
+
+template<int mod>
+struct modint{
+    long long x;
+    modint(long long _x=0) noexcept : x((_x%mod+mod)%mod){}
+    long long val() const noexcept {return x;}
+    modint& operator+=(const modint& a) noexcept {if((x += a.x) >= mod) x -= mod;return *this;}
+    modint& operator-=(const modint& a) noexcept {if((x += mod - a.x) >= mod) x -= mod;return *this;}
+    modint& operator*=(const modint& a) noexcept {(x *= a.x) %= mod;return *this;}
+    modint& operator/=(const modint& a) {return (*this) *= a.inv();}
+    modint& operator++() noexcept {return *this += 1;}
+    modint& operator--() noexcept {return *this -= 1;}
+    modint operator++(int) {modint tmp = *this; ++*this; return tmp;}
+    modint operator--(int) {modint tmp = *this; --*this; return tmp;}
+    modint inv() const {return pow(mod-2);}
+    friend modint operator+(const modint&  t, const modint& a)  {modint tmp = t; return tmp += a;}
+    friend modint operator-(const modint&  t, const modint& a)  {modint tmp = t; return tmp += (a * -1);}
+    friend modint operator*(const modint&  t, const modint& a)  {modint tmp = t; return tmp *= a;}
+    friend modint operator/(const modint&  t, const modint& a)  {assert(a.x != 0) ; return a.inv() *=  t;}
+    modint pow(const long long& y) const {
+        if(!y) return 1;
+        modint res = pow(y >> 1);
+        res *= res;
+        if(y & 1) res *= *this;
+        return res;
+    }
+    friend istream& operator>>(istream& is, modint& a) { return is >> a.x;}
+    friend ostream& operator<<(ostream& os, const modint& a) { return os << a.x;}
+    bool operator==(const modint& a) const {return x == a.x;}
+    bool operator!=(const modint& a) const {return x != a.x;}
+    bool operator<(const modint& a) const {return x < a.x;}
+};
+
+using mint = modint<1000000007>;
+
+/*
+@brief modint1000000007
+*/
